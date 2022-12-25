d53a03b9db5399df21f37dff067766f49983714b
WillBeFront-Enduser <pugbmobile40@gmail.com>
2022-12-25 16:19:20 +0300

    ayayay

--- index.js
+++ index.js
@@ -1,39 +1,68 @@
-const api = "http://www.omdbapi.com/?i=tt3896198&apikey=2a7d7e9f&s"
-const searchbox = document.querySelector(".Search-box");
-// searchbox.addEventListener('keypress',setQuery);
-const button = document.querySelector(".button");
-button.addEventListener('click',setQuery);
+const apiUrl = "http://www.omdbapi.com/?apikey=2a7d7e9f&s";
 
-function setQuery(evt) {
-    getResults(searchbox.value);
+const searchForm = document.querySelector(".Search-box");
+const searchButton = document.querySelector(".button");
+
+// Set up the event listeners
+searchForm.addEventListener("keypress", searchMovies);
+searchButton.addEventListener("click", searchMovies);
+
+function searchMovies(event) {
+  // Only submit the form if the user pressed the Enter key or clicked the button
+  if (event.key === "Enter" || event.type === "click") {
+    // Get the search query from the form
+    const query = searchForm.value;
+
+    // Send a request to the API
+    fetch(`${apiUrl}&s=${query}`)
+      .then((response) => response.json())
+      .then((data) => {
+        // Clear the existing movie results
+        const movieContainer = document.getElementById("content");
+        while (movieContainer.lastElementChild) {
+          movieContainer.removeChild(movieContainer.lastElementChild);
+        }
+
+        // Loop through the movies and display them on the page
+        for (const movie of data.Search) {
+          // Create the movie element
+          const movieElement = document.createElement("div");
+          movieElement.classList.add("box");
+
+          // Add the movie data to the element
+          const title = document.createElement("div");
+          title.textContent = `Title: ${movie.Title},(${movie.Year})`;
+          movieElement.appendChild(title);
+
+          const type = document.createElement("div");
+          type.textContent = `Type: ${movie.Type}`;
+          movieElement.appendChild(type);
+
+          const poster = document.createElement("div");
+          const posterImg = document.createElement("img");
+          posterImg.src = movie.Poster;
+          poster.appendChild(posterImg);
+          movieElement.appendChild(poster);
+
+          const moreinfo = document.createElement("div");
+          const moreinfoLink = document.createElement("a");
+          moreinfoLink.textContent = "More Details";
+          moreinfoLink.target = "_blank";
+          moreinfoLink.href = `https://www.imdb.com/title/${movie.imdbID}`;
+          moreinfo.appendChild(moreinfoLink);
+          movieElement.appendChild(moreinfo);
+
+          // Add the movie element to the page
+          movieContainer.appendChild(movieElement);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        // Display an error message to the user
+        const errorElement = document.createElement("div");
+        errorElement.textContent = "An error occurred while searching for movies. Please try again later.";
+        errorElement.classList.add("error");
+        movieContainer.appendChild(errorElement);
+      });
+  }
 }
-
-function getResults(q) {
-     fetch(`${api}&s=${q}`)
-    .then(movie =>{
-        return movie.json();
-    }).then((movie)=>{
-        // console.log(movie.Search.length);
-        
-            var content = document.querySelector("#content"); 
-            
-            var box = content.lastElementChild;  
-            while (box) { 
-                content.removeChild(box); 
-                box = content.lastElementChild; 
-            } 
-
-            for(var a=0;a<movie.Search.length;a++){
-                let box = document.createElement("div");
-                box.setAttribute("class","box");
-                let content =document.getElementById("content");
-
-                content.appendChild(box);
-
-                box.innerHTML = `<div class="title">Title:- ${movie.Search[a].Title},(${movie.Search[a].Year})</div>
-                <div class="type">Type:-${movie.Search[a].Type}</div>
-                <div class="poster"><img src="${movie.Search[a].Poster}"></img></div>
-                <div class="moreinfo"><a href="https://www.imdb.com/title/${movie.Search[a].imdbID}" target="_blank">More Details</a></div>`;
-            }
-    })
-} 
